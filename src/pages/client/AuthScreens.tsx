d27685681f2e746b6098addd9ae8a30d3884376d
Reynear <douglasreynear@gmail.com>
2025-11-30 21:09:56 -0500

    feat: Add default login credentials for Admin user

--- src/pages/client/AuthScreens.tsx
+++ src/pages/client/AuthScreens.tsx
@@ -138,6 +138,7 @@ function Credentials() {
 			<p class="font-semibold text-gray-700">Default Logins:</p>
 			<p>User → user@mymona.uwi.edu / user123</p>
 			<p>Staff → staff@mymona.uwi.edu / staff123</p>
+			<p>Admin → admin@mymona.uwi.edu / admin123</p>
 		</section>
 	);
 }
